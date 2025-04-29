443d70d7f628cc4bed2ae8199b47ba31469b2c6a
Zoey Fang <205705295+zoeyfang8@users.noreply.github.com>
2025-04-29 15:07:59 -0500

    update

--- data/database-schema.sql
+++ data/database-schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE countries (
     id integer NOT NULL,
     area integer,
     continent integer
-)
+);
 CREATE TABLE flags (
     id integer NOT NULL,
     red	integer,
@@ -13,9 +13,9 @@ CREATE TABLE flags (
     black integer,
     orange integer,
     mainhue text
-)
+);
 
 CREATE TABLE countries_flags (
     country_id integer,
     flag_id integer
-)
+);
