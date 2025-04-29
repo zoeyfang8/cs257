8db7e565e22ee455078db1cd671b426bfbed8dfc
Zoey Fang <205705295+zoeyfang8@users.noreply.github.com>
2025-04-29 15:06:30 -0500

    added tables

--- data/database-schema.sql
+++ data/database-schema.sql
@@ -1,1 +1,21 @@
-CREATE TABLE
+CREATE TABLE countries (
+    id integer NOT NULL,
+    area integer,
+    continent integer
+)
+CREATE TABLE flags (
+    id integer NOT NULL,
+    red	integer,
+    green integer,
+    blue integer,
+    gold integer,
+    white integer,
+    black integer,
+    orange integer,
+    mainhue text
+)
+
+CREATE TABLE countries_flags (
+    country_id integer,
+    flag_id integer
+)
