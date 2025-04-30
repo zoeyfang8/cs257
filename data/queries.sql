19e0a1e14881cb40d8c32aafedaba231a2a1aac3
Zoey Fang <205705295+zoeyfang8@users.noreply.github.com>
2025-04-29 20:34:18 -0500

    added

--- data/queries.sql
+++ data/queries.sql
@@ -1,1 +1,17 @@
+SELECT flags.name
+FROM flags, flags_countries
+WHERE flags.red = '1'
+AND flags.id = flags_countries.flag_id;
 
+
+SELECT countries.name, countries.continent, flags.mainhue
+FROM flags, countries, flags_countries
+WHERE flags.id = flags_countries.flag_id
+AND  countries.id = flags_countries.country_id;
+
+
+SELECT countries.name, countries.area
+FROM countries, flags_countries
+WHERE flags.id = flags_countries.flag_id
+AND countries.id = flags_countries.country_id
+AND countries.area > 500
