cda61665736bcf637355f5013e1d63e4cb5a9aaa
Zoey Fang <205705295+zoeyfang8@users.noreply.github.com>
2025-04-30 22:06:12 -0500

    updated

--- data/queries.sql
+++ data/queries.sql
@@ -7,11 +7,12 @@ AND flags.id = flags_countries.flag_id;
 SELECT countries.countryname, countries.continent, flags.mainhue
 FROM flags, countries, flags_countries
 WHERE flags.id = flags_countries.flag_id
-AND  countries.id = flags_countries.country_id;
+AND  countries.id = flags_countries.country_id
+AND countries.continent = 1;
 
 
 SELECT countries.countryname, countries.area
-FROM countries, flags_countries
+FROM countries, flags, flags_countries
 WHERE flags.id = flags_countries.flag_id
 AND countries.id = flags_countries.country_id
-AND countries.area > 500
+AND countries.area > 500;
